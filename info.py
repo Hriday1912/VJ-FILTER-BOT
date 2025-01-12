e3ed1f933ba64ee1c36a727f2ea4375ebd985583
Hriday1912 <jhriday9@gmail.com>
2025-01-12 10:20:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,10 +88,10 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/vj_bot_disscussion')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vj_botz')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/kingvj01')
+GRP_LNK = environ.get('GRP_LNK', https://t.me/searchMoviebysuperviewer'')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Superviewerstar')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'searchMoviebysuperviewerbot') # Support Chat Link Without https:// or @
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Superviewerstar')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
