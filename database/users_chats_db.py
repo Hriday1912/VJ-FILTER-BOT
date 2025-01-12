d0b8237f11edb7abb23b3d9ae021e3cadb6e61d6
Hriday1912 <jhriday9@gmail.com>
2025-01-12 14:24:42 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -10,7 +10,8 @@ from info import DATABASE_NAME, USER_DB_URI, OTHER_DB_URI, CUSTOM_FILE_CAPTION, 
 import time
 import datetime
 
-my_client = MongoClient(OTHER_DB_URI)
+my_client = MongoClient(mongodb+srv://HRIDAY19:Hriday123456@@@superviewer.mongodb.net/database
+)
 mydb = my_client["referal_user"]
 
 async def referal_add_user(user_id, ref_user_id):
