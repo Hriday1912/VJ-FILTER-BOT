e3e06e8982c909886876354a68fe8bf02c7a26b1
Hriday1912 <jhriday9@gmail.com>
2025-01-12 11:18:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'Super Viewer')
 API_ID = int(environ.get('API_ID', 29544183''))
 API_HASH = environ.get('API_HASH', df920dc2aaa645a3aa6674ba223768b9'')
-BOT_TOKEN = environ.get('BOT_TOKEN', 7792844100:AAF4Eo3973UcbylNXAYYZ00zWwvOJkdQ35U"")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
